82f6661fb0622792363ff49b399e4756fb15970f
miho yamagata <miho.yamagata@gmail.com>
2020-09-21 12:33:02 +0900

    practiced React component

--- src/WithJsx.js
+++ src/WithJsx.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 export default class extends Component {
   render() {
     return (
       <main>
-        <div className="test">
+        <div className="subscription">
           <p className="explanation">これはテストの説明です。</p>
         </div>
       </main>
-    );
+    )
   }
 }
+
